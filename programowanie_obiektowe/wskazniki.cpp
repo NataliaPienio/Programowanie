4b6dd6c8c31f6b723992997294cbf0972e4cce31
NataliaPienio <90185181+NataliaPienio@users.noreply.github.com>
2021-10-06 15:42:04 +0200

    Add files via upload

---
+++ programowanie_obiektowe/wskazniki.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+class Worker {
+public:
+    string name, surname;
+    void getData();
+};
+
+void Worker::getData(){
+    cout<<"ImiÍ i nazwisko: "<<name<<" "<<surname;
+};
+
+int main(int argc, char** argv){
+    setlocale(LC_CTYPE,"polish");
+    Worker kowalski;
+
+    Worker *p_kowalski;
+
+    p_kowalski=&kowalski;
+    cout<<"Adres obiektu kowalski: "<<p_kowalski;
+
+    kowalski.surname="Kowalski";
+    p_kowalski->name="Pawe≥";
+
+    kowalski.getData();
+    p_kowalski->getData();//wskaünik
+
+
+    return 0;
+}
